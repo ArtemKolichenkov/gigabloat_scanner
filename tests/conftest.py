c29546bda9ae7625033922a44a5210295481a8db
Artem Kolichenkov <artem@kolichenkov.com>
2020-05-09 15:24:53 +0300

    Create empty dir in test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from .expected_results import setup_mocks
 PATH_TO_TEST = os.path.abspath("tests/test_files")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def remove_ds_stores():
     stores = Path(PATH_TO_TEST).rglob(".DS_Store")
     for store_file in stores:
@@ -16,14 +16,17 @@ def remove_ds_stores():
     yield
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def scan_result():
+    empty_dir = PATH_TO_TEST + "/EmptyDir"
+    if not os.path.isdir(empty_dir):
+        os.makedirs(empty_dir)
     test_scanner = gigabloat.Scanner(PATH_TO_TEST)
     test_scanner.full_scan()
     return test_scanner
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def expected_root():
     root = setup_mocks()
     return root
